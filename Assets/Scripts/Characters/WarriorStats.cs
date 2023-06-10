6cc1caeab11f938a6bc4712dc8c0515d6fff9896
radiadus <55176713+radiadus@users.noreply.github.com>
2023-06-11 01:26:10 +0700

    Add Skill to CharacterStats

--- Assets/Scripts/Characters/WarriorStats.cs
+++ Assets/Scripts/Characters/WarriorStats.cs
@@ -18,7 +18,9 @@ public class WarriorStats : Stats
         speed = 10;
         skillList = new List<Skill>
         {
-            new P3K()
+            new P3K(),
+            new Potong(),
+            new Provokasi(),
         };
     }
 
@@ -26,13 +28,20 @@ public class WarriorStats : Stats
     {
         skillList = new List<Skill>();
         skillList.Add(new P3K());
+        skillList.Add(new Potong());
+        skillList.Add(new Provokasi());
         if (PlayerPrefs.GetInt("warrior2", 0) == 1)
         {
             //initialize tier 2 warrior skills
+            skillList.Add(new SemangatPemuda());
+            skillList.Add(new TebasanLebar());
+            skillList.Add(new PosisiJaga());
         }
         if (PlayerPrefs.GetInt("warrior3", 0) == 1)
         {
             //initialize tier 3 warrior skills
+            skillList.Add(new SeranganBalik());
+            skillList.Add(new AmukanPahlawan());
         }
     }
 }
