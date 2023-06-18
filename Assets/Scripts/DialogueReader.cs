1f53da9c6dd7958380a1cb8f9aa5aa04daad59c8
CovenZy <bonaventura.jason@gmail.com>
2023-06-18 19:45:51 +0700

    cutscene scenes and confirmation buttons on flee and new game

--- Assets/Scripts/DialogueReader.cs
+++ Assets/Scripts/DialogueReader.cs
@@ -20,7 +20,7 @@ public class DialogueReader : MonoBehaviour
     private Dictionary<string, Dictionary<int, List<string>>> FetchDialogues()
     {
         Dictionary<string, Dictionary<int, List<string>>> dialogues = new Dictionary<string, Dictionary<int, List<string>>>();
-        StreamReader file = File.OpenText("Assets/Resources/NPC Dialogues.csv");
+        StreamReader file = File.OpenText("Assets/Resources/NPC Dialogues 2.csv");
         string line = "";
         while ((line = file.ReadLine()) != null)
         {
